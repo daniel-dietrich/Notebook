ac8ace24875cde161ede665e77be156f28ac1cba
Daniel Dietrich <daniel.dietrich@tutamail.com>
2021-04-28 20:35:21 +0200

    Added icon

--- src/main/Notebook.java
+++ src/main/Notebook.java
@@ -1,6 +1,7 @@
 package main;
 
 import controllers.MainPaneController;
+import javafx.scene.image.Image;
 import models.MainPaneModel;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -21,14 +22,16 @@ public class Notebook extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../views/MainPane.fxml"));
         fxmlLoader.setControllerFactory(t -> new MainPaneController(new MainPaneModel()));
+        Image image = new Image("icon.png");
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add("stylesheets/light-theme.css");
-        Notebook.stage = stage;
 
+        stage.getIcons().add(image);
         stage.setTitle("Notebook");
-
         stage.setScene(scene);
         stage.show();
+
+        Notebook.stage = stage;
     }
 
     public static Scene getScene() {
